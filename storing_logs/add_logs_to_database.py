7a29ffa7fc4b844237fa158d6eefb95a653010d1
Istvan Takacs <isti.takacs0@gmail.com>
2023-07-17 22:16:19 +0100

    Logs stored in MongoDB and accessed. Started probability distribution.

--- storing_logs/add_logs_to_database.py
+++ storing_logs/add_logs_to_database.py
@@ -6,53 +6,70 @@ from pprint import pprint
 import mongo_connect
 import pymongo
 
+  
+def get_main_log_collection():
+    data_main = pyparsing_logs.pyparse_logs()
 
-data_main = pyparsing_logs.pyparse_logs()
-data_network = pyparsing_logs_network.pyparse_logs()
+    # Get a client connection to the MongoDB database.
+    client = mongo_connect.get_client()
 
-# print(data_network)
+    # Create a connection to the database.
+    db = client[mongo_connect.get_database_name()]
 
-# Get a client connection to the MongoDB database.
-client = mongo_connect.get_client()
+    # Create collections.
+    main_log_collection = db['MainLogs']
 
-# Create a connection to the database.
-db = client[mongo_connect.get_database_name()]
+    # Create unique indexes so that no duplicate logs are accepted
+    main_log_collection.create_index([("index", pymongo.ASCENDING)] , unique=True)
 
-# Create collections.
-main_log_collection = db['MainLogs']
-network_log_collection = db["NetworkLogs"]
+    # Insert the documents into the collection.
+    i = 0
+    for index in data_main:
+        try:
+            result = main_log_collection.insert_one(data_main[i])
+            i += 1
 
-# Create unique indexes so that no duplicate logs are accepted
-main_log_collection.create_index([("index", pymongo.ASCENDING)] , unique=True)
-network_log_collection.create_index([("index", pymongo.ASCENDING)] , unique=True)
+        except pymongo.errors.DuplicateKeyError as dpk:
+            pass
+
+    return list([dicts for dicts in main_log_collection.find()])
 
 
-# Insert the documents into the collection.
-i = 0
-for index in data_main:
-    try:
-        result = main_log_collection.insert_one(data_main[i])
-        i += 1
+def get_network_log_collection():
+    data_network = pyparsing_logs_network.pyparse_logs()
 
-    except pymongo.errors.DuplicateKeyError as dpk:
-        pass
+    # Get a client connection to the MongoDB database.
+    client = mongo_connect.get_client()
 
-i = 0
-for index in data_network:
-    try:
-        result = network_log_collection.insert_one(data_network[i])
-        i += 1
+    # Create a connection to the database.
+    db = client[mongo_connect.get_database_name()]
 
-    except pymongo.errors.DuplicateKeyError as dpk:
-        pass
+    # Create collections.
+    network_log_collection = db["NetworkLogs"]
 
+    # Create unique indexes so that no duplicate logs are accepted
+    network_log_collection.create_index([("index", pymongo.ASCENDING)] , unique=True)
+
+    # Insert the documents into the collection.
+    i = 0
+    for index in data_network:
+        try:
+            result = network_log_collection.insert_one(data_network[i])
+            i += 1
+
+        except pymongo.errors.DuplicateKeyError as dpk:
+            pass 
+
+    return list([dicts for dicts in network_log_collection.find()])
  
 
 # print(result.inserted_ids)
-print(">> Documents in the \"MainLogs\" collection:")
-print(db["MainLogs"].count_documents({}))
-print(">> Documents in the \"NetworkLogs\" collection:")
-print(db["NetworkLogs"].count_documents({}))
+# print(">> Documents in the \"MainLogs\" collection:")
+# print(db["MainLogs"].count_documents({}))
+# print(">> Documents in the \"NetworkLogs\" collection:")
+# print(db["NetworkLogs"].count_documents({}))
+
+
 
 # print(db["MainLogs"].distinct("appname"))
-
+    
